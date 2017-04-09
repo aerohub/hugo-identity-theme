a128e853ecdb84a9cd8e96528a0b04ef3dcae974
Michael Grosser <development@stp-ip.net>
2017-04-09 20:25:19 +0000

    Improve contact form validation

---
+++ static/assets/js/contact.js
@@ -0,0 +1,19 @@
+// Contact form validation
+$.validate({
+  modules : 'html5, toggleDisabled'
+});
+
+// Async contact form
+$('form[id=contactForm]').submit(function(){
+  $.post($(this).attr('action'), $(this).serialize(), function(res){
+    $('form[id=contactForm] #success').hide();
+    $('form[id=contactForm] #error').hide();
+    if (res.code == "200")
+      $('form[id=contactForm] #success').show();
+    }).fail(function(){
+    $('form[id=contactForm] #success').hide();
+    $('form[id=contactForm] #error').hide();
+    $('form[id=contactForm] #error').show();
+  });
+  return false;
+});

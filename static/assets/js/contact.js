fd70d9fa1cd30c16a17d0830e2d21f0667e77f64
Michael Grosser <development@stp-ip.net>
2017-09-02 16:56:31 +0000

    Add edgecase handling to contact form

--- static/assets/js/contact.js
+++ static/assets/js/contact.js
@@ -12,10 +12,28 @@ $('form[id=contactForm]').change(function() {
   });
 });
 
-// Async contact form
-$('form[id=contactForm]').submit(function(){
-  $('form[id=contactForm] #success').hide();
+function clear() {
   $('form[id=contactForm] #error').hide();
+  $('form[id=contactForm] #success').hide();
+  $('form[id=contactForm] #val-error').hide();
+}
+
+// Async contact form
+$('form[id=contactForm]').submit(function(e){
+  e.preventDefault();
+  clear();
+
+  if ($('.g-recaptcha').length) {
+    if (typeof grecaptcha != "undefined") {
+      if (!grecaptcha.getResponse()) {
+        $('form[id=contactForm] #val-error').show();
+        return;
+      }
+    } else {
+      $('form[id=contactForm] #val-error').show();
+      return;
+    }
+  }
 
   // AJAX POST, with contact form data serialized, which expects a JSON response
   // Datatype must be set to 'json' to use formspree.io
@@ -24,13 +42,9 @@ $('form[id=contactForm]').submit(function(){
     }, 'json').fail(function(){
       $('form[id=contactForm] #error').show();
   });
-  return false;
 });
 
 // Callback function for captcha
 function onSubmit(token) {
-  if (!token) {
-     return;
-  }
   $('form[id=contactForm]').submit();
 }
